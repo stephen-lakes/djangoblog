4d7969a6e1fc7f83e08bb27a60b1b71504b05be3
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-03-06 18:51:41 +0100

    updated tests to pass the changes made to post detail

--- blog/tests.py
+++ blog/tests.py
@@ -41,4 +41,4 @@ class BlogTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
-        self.assertTemplateUsed(response, 'detail.html')
+        self.assertTemplateUsed(response, 'post_detail.html')

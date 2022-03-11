0e81e710dc7fbb32c95ae8c2e4157be4574b0f9f
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-03-11 01:48:45 +0100

    added new path for delete view

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
     path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
-    path('post/update/<int:pk>', views.BlogUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/update/', views.BlogUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete', views.BlogDeleteView.as_view(), name='post_delete'),
 ]
